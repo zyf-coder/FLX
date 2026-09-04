89aec6a5983853c1fa577af833d59bd9b31318ad
zyf <2409352207@qq.com>
2026-09-04 17:26:46 +0800

    修复云端媒体存储权限

--- supabase/setup.sql
+++ supabase/setup.sql
@@ -77,12 +77,6 @@ on conflict (id) do update set public = true;
 drop policy if exists "couple photo upload" on storage.objects;
 drop policy if exists "couple photo delete" on storage.objects;
 create policy "couple photo upload" on storage.objects
-for insert to anon with check (
-  bucket_id = 'couple-photos'
-  and (storage.foldername(name))[1] = (current_setting('request.headers', true)::json ->> 'x-couple-id')
-);
+for insert to anon with check (bucket_id = 'couple-photos');
 create policy "couple photo delete" on storage.objects
-for delete to anon using (
-  bucket_id = 'couple-photos'
-  and (storage.foldername(name))[1] = (current_setting('request.headers', true)::json ->> 'x-couple-id')
-);
+for delete to anon using (bucket_id = 'couple-photos');
